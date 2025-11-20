b79f4c5be77cd547767168d7a084e2f1368f8470
pxlmvr <matteo.losi@thevisualagency.com>
2025-11-20 12:18:00 +0100

    chore: update build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import dts from "vite-plugin-dts";
 
 export default defineConfig({
-  plugins: [react(), dts()],
+  plugins: [react()],
   build: {
     lib: {
       entry: "src/index.ts",
