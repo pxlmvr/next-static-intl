5332e93cf4ff3c89eddc1ed8dd55dc8a435b76ef
pxlmvr <matteo.losi@thevisualagency.com>
2025-11-21 10:00:00 +0100

    chore: tune building options

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { createT } from './createT'
 export { TranslationProvider } from './TranslationProvider'
 export { useTranslations } from './useTranslations'
+export { createT } from './createT'
 export * from './types'
